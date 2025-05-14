6be5b2504257708b374c296b7f7ec0d909d097ec
squallmar <marcelmendes05@gmail.com>
2025-05-13 21:58:59 -0300

    dia 13-05 21:58h

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,8 +1,11 @@
 class RankingsController < ApplicationController
   def index
-    @users = User.left_joins(:bets)
-                 .group("users.id")
-                 .order("SUM(bets.points) DESC")
-                 .select("users.*, SUM(bets.points) as total_points")
+    @rankings = User.left_joins(:bets)
+                   .select('users.*, 
+                           SUM(bets.points) as total_points,
+                           COUNT(bets.id) as bets_count,
+                           SUM(CASE WHEN bets.points > 0 THEN 1 ELSE 0 END) as correct_bets_count')
+                   .group('users.id')
+                   .order('total_points DESC NULLS LAST')
   end
 end
