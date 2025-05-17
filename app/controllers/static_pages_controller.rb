46ad76055753f0f3dca7a10f276afde461e226e7
squallmar <marcelmendes05@gmail.com>
2025-05-16 21:16:51 -0300

    21:16

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,5 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!
-
+  
   def termos
     @page_title = "Termos de Uso"
   end
--- app/helpers/rankings_helper.rb
+++ app/helpers/rankings_helper.rb
@@ -1,10 +1,10 @@
 module RankingsHelper
   def accuracy_color(accuracy)
     case accuracy
-    when 80..100 then 'success'
-    when 60...80 then 'info'
-    when 40...60 then 'warning'
-    else 'danger'
+    when 80..100 then "success"
+    when 60...80 then "info"
+    when 40...60 then "warning"
+    else "danger"
     end
   end
 end
